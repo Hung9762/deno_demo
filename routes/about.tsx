67894e9d0742f3efe4ace3ec3d1bc7eaf389fe4f
Alexander Hung <hung9762@gmail.com>
2024-11-24 23:09:00 -0600

    deno demo

---
+++ routes/about.tsx
@@ -0,0 +1,18 @@
+import { FreshContext, Handlers } from "$fresh/server.ts";
+
+export const handler: Handlers = {
+    async GET(_req: Request, ctx: FreshContext) {
+        const resp = await ctx.render();
+        resp.headers.set("X-Custom-Header", "Hello");
+        return resp;
+    },
+};
+
+export default function AboutPage() {
+    return (
+        <main>
+            <h1>About</h1>
+            <p>This is the about page.</p>
+        </main>
+    );
+}
